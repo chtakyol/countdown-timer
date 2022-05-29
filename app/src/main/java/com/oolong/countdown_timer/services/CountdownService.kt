a798932ba92630ff20c829aee2f125d48f30f700
DESKTOP-3GJO0QV\Cihat <chtakyol@gmail.com>
2022-05-29 23:30:50 +0300

    Notification action button added.

--- app/src/main/java/com/oolong/countdown_timer/services/CountdownService.kt
+++ app/src/main/java/com/oolong/countdown_timer/services/CountdownService.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
 import android.content.Intent
 import android.os.Build
@@ -15,6 +16,7 @@ import androidx.core.app.NotificationManagerCompat
 import androidx.lifecycle.LifecycleService
 import com.oolong.countdown_timer.MainActivity
 import com.oolong.countdown_timer.R
+import com.oolong.countdown_timer.utils.Constants.ACTION_ADD_BUTTON
 import com.oolong.countdown_timer.utils.Constants.ACTION_PAUSE_SERVICE
 import com.oolong.countdown_timer.utils.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.oolong.countdown_timer.utils.Constants.ACTION_STOP_SERVICE
@@ -46,6 +48,11 @@ class CountdownService: LifecycleService() {
         .setContentTitle("Background Timer")
         .setContentText(Utilities.getNotificationText(durationInMillis.value))
 
+    override fun onCreate() {
+        super.onCreate()
+        notificationBuilder
+            .addAction(R.drawable.ic_baseline_timer_24, "Add 10 min", addDurationPendingIntent())
+    }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
@@ -65,6 +72,9 @@ class CountdownService: LifecycleService() {
                 }
                 ACTION_STOP_SERVICE -> {
                     Log.d("Service", "Service stop")
+                }
+                ACTION_ADD_BUTTON -> {
+                    setTimerDuration(CountdownService.durationInMillis.value + 1000 * 60 * 10)
                 }
                 else -> {
                     Log.d("Service", "Unexpected command")
@@ -107,6 +117,14 @@ class CountdownService: LifecycleService() {
         this,
         0,
         Intent(this, MainActivity::class.java),
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
+
+    @SuppressLint("UnspecifiedImmutableFlag")
+    private fun addDurationPendingIntent() = PendingIntent.getService(
+        this,
+        2,
+        Intent(this, CountdownService::class.java).apply { action = ACTION_ADD_BUTTON },
         PendingIntent.FLAG_UPDATE_CURRENT
     )
 
--- app/src/main/java/com/oolong/countdown_timer/utils/Constants.kt
+++ app/src/main/java/com/oolong/countdown_timer/utils/Constants.kt
@@ -12,6 +12,7 @@ object Constants {
     const val ACTION_START_OR_RESUME_SERVICE = "ACTION_START_OR_RESUME_SERVICE"
     const val ACTION_PAUSE_SERVICE = "ACTION_PAUSE_SERVICE"
     const val ACTION_STOP_SERVICE = "ACTION_SPOT_SERVICE"
+    const val ACTION_ADD_BUTTON = "ACTION_ADD_BUTTON"
 
     const val NOTIFICATION_CHANNEL_ID = "countdown_channel"
     const val NOTIFICATION_CHANNEL_NAME = "Countdown"
